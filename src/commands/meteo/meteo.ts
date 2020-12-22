189d32188fbb564f9dc4a2331b4eccc2f18b1439
Yosoydead <radu-bogdan@outlook.com>
2020-12-22 11:19:17 +0200

    adaugat optiune pentru codul tarii

--- src/commands/meteo/meteo.ts
+++ src/commands/meteo/meteo.ts
@@ -2,6 +2,6 @@ import { getOpenWeatherData } from "../../services/openWeather/get";
 import { IFetchClient } from "../../services/FetchClient";
 import { MessageEmbed } from "discord.js";
 
-export async function meteo(client: IFetchClient, appKey: string, city: string): Promise<MessageEmbed> {
-  return await getOpenWeatherData(client, appKey, city);
+export async function meteo(client: IFetchClient, appKey: string, city: string, countryCode: string = ""): Promise<MessageEmbed> {
+  return await getOpenWeatherData(client, appKey, city, countryCode);
 }
